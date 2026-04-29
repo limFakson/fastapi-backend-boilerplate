bd7543588f0da04c01ab482e96ac5c7d955c56c8
Joshua Opeyemi <119635207+limFakson@users.noreply.github.com>
2026-04-29 17:36:11 +0100

    Update health.py

--- api/endpoints/health.py
+++ api/endpoints/health.py
@@ -1,11 +1,8 @@
 from fastapi import APIRouter
-
+from core.response import success_response
 router = APIRouter()
 
 
 @router.get("/")
 def health_check():
-    return {
-        "status": "ok",
-        "message": "API is healthy"
-    }
+    return success_response(status= "ok", message="API is healthy", data="")
